924f64ad37105defd30139c1a1e75be0d5440e06
Kim Hayeong <gkdud9209@gmail.com>
2025-09-26 17:58:51 +0900

    feat: 로그인 기능 구축

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { RiFootprintFill } from "react-icons/ri";
 import { HiOutlinePencilSquare } from "react-icons/hi2";
-
-
+import { login, logout } from "../api/firebase";
 
 export default function NavBar() {
+  const [user, setUser] = useState();
+  const handleLogin = () => {
+    login().then(setUser);
+  };
+  const handleLogout = () => {
+    logout().then(setUser);
+  };
   return (
-    <header className='flex justify-between border-b border-gray-300 p-2'>
-        <Link to='/' className='flex items-center text-4xl text-brand'>
-            <RiFootprintFill />
-            <h1>STEPIE</h1>
+    <header className="flex justify-between border-b border-gray-300 p-2">
+      <Link to="/" className="flex items-center text-4xl text-brand">
+        <RiFootprintFill />
+        <h1>STEPIE</h1>
+      </Link>
+      <nav className="flex items-center gap-4 font-semibold">
+        <Link to="/products">Products</Link>
+        <Link to="/carts">Carts</Link>
+        <Link to="/products/new" className="text-2xl">
+          <HiOutlinePencilSquare />
         </Link>
-        <nav className='flex items-center gap-4 font-semibold'>
-            <Link to='/products'>Products</Link>
-            <Link to='/carts'>Carts</Link>
-            <Link to='/products/new' className='text-2xl'>
-                <HiOutlinePencilSquare />
-            </Link>
-            <button>Login</button>
-        </nav>
+        {!user && <button onClick={handleLogin}>Login</button>}
+        {user && <button onClick={handleLogout}>Logout</button>}
+      </nav>
     </header>
-  )
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import Home from './pages/Home';
-import AllProducts from './pages/AllProducts';
-import ProductDetail from './pages/ProductDetail';
-import NewProduct from './pages/NewProduct';
-import MyCart from './pages/MyCart';
-import NotFound from './pages/NotFound';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Home from "./pages/Home";
+import AllProducts from "./pages/AllProducts";
+import ProductDetail from "./pages/ProductDetail";
+import NewProduct from "./pages/NewProduct";
+import MyCart from "./pages/MyCart";
+import NotFound from "./pages/NotFound";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <NotFound />,
     children: [
-      { index: true, path: '/', element: <Home />},
-      {path: '/products', element: <AllProducts />},
-      {path: '/products/new', element: <NewProduct />},
-      {path: '/products/:id', element: <ProductDetail />},
-      {pathL: '/carts', element: <MyCart />}
-    ]
-  }
+      { index: true, path: "/", element: <Home /> },
+      { path: "/products", element: <AllProducts /> },
+      { path: "/products/new", element: <NewProduct /> },
+      { path: "/products/:id", element: <ProductDetail /> },
+      { pathL: "/carts", element: <MyCart /> },
+    ],
+  },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
