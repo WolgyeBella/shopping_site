2e13449281d08c461c09ea5cc0060770563e429b
Kim Hayeong <gkdud9209@gmail.com>
2025-09-30 14:27:35 +0900

    feat: 어드민 ui 및 경로보호 설정

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,16 +4,11 @@ import { RiFootprintFill } from "react-icons/ri";
 import { HiOutlinePencilSquare } from "react-icons/hi2";
 import { login, logout, onUserStateChange } from "../api/firebase";
 import User from "./User";
+import Button from "./ui/Button";
+import { useAuthContext } from "./context/AuthContext";
 
 export default function NavBar() {
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    onUserStateChange((user) => {
-      console.log(user);
-      setUser(user);
-    });
-  }, []);
+  const { user, login, logout } = useAuthContext();
 
   return (
     <header className="flex justify-between border-b border-gray-300 p-2">
@@ -23,13 +18,15 @@ export default function NavBar() {
       </Link>
       <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>
-        <Link to="/carts">Carts</Link>
-        <Link to="/products/new" className="text-2xl">
-          <HiOutlinePencilSquare />
-        </Link>
+        {user && <Link to="/carts">Carts</Link>}
+        {user && user.isAdmin && (
+          <Link to="/products/new" className="text-2xl">
+            <HiOutlinePencilSquare />
+          </Link>
+        )}
         {user && <User user={user} />}
-        {!user && <button onClick={login}>Login</button>}
-        {user && <button onClick={logout}>Logout</button>}
+        {!user && <Button text={"Login"} onClick={login} />}
+        {user && <Button text={"Logout"} onClick={logout} />}
       </nav>
     </header>
   );
